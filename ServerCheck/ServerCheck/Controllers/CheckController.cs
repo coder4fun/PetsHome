2473011e2b1cfaa38d3879d4b844b1061f3054b6
Panuganti N. Saikiran <psaikiran@pc.factset.com>
2017-10-29 04:37:59 +0530

    Pushing sample server check code

---
+++ ServerCheck/ServerCheck/Controllers/CheckController.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace ServerCheck.Controllers
+{
+    public class CheckController : ApiController
+    {
+        private const string MyConnectinString = "Server=183.82.121.94;Port=3306;Database=Test;Uid=root;Pwd=PetsDatebase@1;";
+
+        [HttpGet]
+        [Route("Web")]
+        public HttpResponseMessage ApplicationStatus()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent("Web application working fine", Encoding.UTF8, "application/json");
+            return response;
+        }
+
+        [HttpGet]
+        [Route("SQL")]
+        public HttpResponseMessage SQLStatus()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(MyConnectinString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = "SELECT * FROM FirstTable LIMIT 1";
+                    string FirstRow = cmd.ExecuteScalar().ToString();
+
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+                    response.Content = new StringContent("SQL Connected successfully. First row of First table is " + FirstRow, Encoding.UTF8, "application/json");
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.InternalServerError);
+
+                response.Content = new StringContent("SQL Connection failed. Exception is " + JsonConvert.SerializeObject(ex), Encoding.UTF8, "application/json");
+                return response;
+            }
+        }
+    }
+}
---
+++ ServerCheck/ServerCheck/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServerCheck.WebApiApplication" Language="C#" %>
